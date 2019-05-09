55c85b52829885d0fbe16d5c2bd5a7c80e6a5e06
Adam Lovastyik <adam@lovastyik.hu>
2019-05-09 13:11:55 +0200

    Fixed to display results in app

--- app/src/main/java/nl/xelion/gameoflife/MainActivity.java
+++ app/src/main/java/nl/xelion/gameoflife/MainActivity.java
@@ -2,7 +2,9 @@ package nl.xelion.gameoflife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.widget.TextView;
 
 import nl.xelion.core.CellBoard;
 import nl.xelion.core.Coordinate;
@@ -10,11 +12,30 @@ import nl.xelion.core.GameOfLife;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        textView.setText("");
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+
+        playXelionGame();
+    }
+
+    private void playXelionGame() {
+
+        textView.setText("");
 
         Coordinate[] seed = new Coordinate[] {new Coordinate(2, 0), new Coordinate(0, 2), new Coordinate(1, 2), new Coordinate(1, 3), new Coordinate(3,3)};
         GameOfLife game = new GameOfLife(4, 4, seed);
@@ -22,9 +43,11 @@ public class MainActivity extends AppCompatActivity {
         int iteration = 0;
         boolean go = true;
 
-        Log.d("GAMEOFLIFE", "Starting layout");
+        Log.d("GAMEOFLIFE", "Starting layout\n");
+        String text = "Starting layout\n";
         CellBoard currentBoard = game.getCurrentBoard();
         Log.d("GAMEOFLIFE", currentBoard != null ? currentBoard.description() : "NULL");
+        text += "\n" + currentBoard != null ? currentBoard.description() : "NULL";
 
         while (iteration < 99 && go) {
 
@@ -33,11 +56,15 @@ public class MainActivity extends AppCompatActivity {
                 go = false;
             }
 
-            Log.d("GAMEOFLIFE", iteration + ". iteration layout");
+            Log.d("GAMEOFLIFE", iteration + ". iteration layout\n");
+            text += "\n" + iteration + ". iteration layout\n";
             CellBoard _currentBoard = game.getCurrentBoard();
             Log.d("GAMEOFLIFE", _currentBoard != null ? _currentBoard.description() : "NULL");
+            text += "\n" + _currentBoard != null ? _currentBoard.description() : "NULL" + "\n";
 
             iteration++;
         }
+
+        textView.setText(text);
     }
 }
